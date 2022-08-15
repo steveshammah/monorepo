0c1de4c98ec6d0e56599d37d4dd65cdddef37ccc
steveshammah <shammahodhiambo@gmail.com>
2022-08-15 20:28:30 +0300

    feat: add next-auth
    - github signin support
    - google signin support

---
+++ apps/store/pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+    // ...add more providers here
+  ],
+});
--- apps/store/pages/index.tsx
+++ apps/store/pages/index.tsx
@@ -3,6 +3,8 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Card from '../components/Card';
 import { useStore } from '../state/loginState';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const getUsers = async () => {
   return axios
@@ -10,8 +12,12 @@ const getUsers = async () => {
     .then((res) => res.data);
 };
 
+const imageUrl = 'https://picsum.photos/200/300/?random';
 export function Index() {
   const { isAuth, login, logout } = useStore();
+  const { data: session } = useSession();
+
+  // const router = useRouter();
 
   const { data, isLoading, error, isFetching } = useQuery(['users'], getUsers);
 
@@ -20,36 +26,58 @@ export function Index() {
   if (error) return 'An error has occurred: ' + error?.message;
 
   return (
-    <div className="bg-gray-50 ">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block"> Welcome to the App 👀</span>
+    <div className="bg-gray-50">
+      <div className="mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between ">
+        <div className="flex-column ">
+          <div className="mb-5">
+            <img
+              src={session ? session.user.image : imageUrl}
+              alt="user profile"
+              className="h-24 w-24 rounded-full shadow-xl"
+            />
+            <h2 className="text-black-800 font-bold text-lg">
+              {session?.user.name}
+            </h2>
+            <p className="text-gray-500 text-sm">{session?.user?.email}</p>
+          </div>
+          <h2 className="font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block text-2xl"> Welcome to the App 👀</span>
 
-          <div>{isFetching ? 'Updating...' : ''}</div>
-        </h2>
+            <div className="text-2xl">{isFetching ? 'Updating...' : ''}</div>
+          </h2>
+        </div>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <div className="ml-3 inline-flex rounded-md shadow">
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
-              onClick={() => logout()}
-            >
-              Logout
-            </a>
-          </div>
-          <div className="inline-flex rounded-md shadow">
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-800 hover:bg-purple-600"
-              onClick={() => login()}
-            >
-              Login
-            </a>
-          </div>
+          {session ? (
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                onClick={() => {
+                  signOut();
+                  logout();
+                }}
+              >
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-800 hover:bg-purple-600"
+                onClick={() => {
+                  signIn();
+                  login();
+                }}
+              >
+                Login
+              </a>
+            </div>
+          )}
         </div>
       </div>
 
-      {isAuth && (
+      {session && (
         <>
           <div className="p-2 bg-gray-200 ">
             <h2 className="text-xl font-black block">Users</h2>
